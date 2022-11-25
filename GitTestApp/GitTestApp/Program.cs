8ed567469105230094495fc471d11f76c57b18fd
daeyeon0821 <daeyeon1535@naver.com>
2022-11-25 16:48:29 +0900

    [Gamma] func: Add new func - SetInch

--- GitTestApp/GitTestApp/Program.cs
+++ GitTestApp/GitTestApp/Program.cs
@@ -19,10 +19,12 @@ namespace GitTestApp
     {
         private const float ONE_INCH = 2.54F;
         public int Centimeter { get; set; } = 0;
+
+        // {    2022. 11. 25 Add function - private SetInch | Gamma
         public float Inch
         {
             get { return Centimeter * ONE_INCH; }
-            private set { Centimeter = (int)(value / ONE_INCH); }
+            private set { this._SetInch(value); }
         }
         public Ruler(int cmValue) { Centimeter = cmValue; }
 
@@ -30,5 +32,11 @@ namespace GitTestApp
         {
             Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
         }
+
+        private void _SetInch(float inchValue) 
+        { 
+            Centimeter = (int)(inchValue / ONE_INCH); 
+        }
+        // }    2022. 11. 25 Add function - private SetInch | Gamma
     }
 }
